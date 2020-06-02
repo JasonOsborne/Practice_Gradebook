fcf5b0464201e1c11e8a5a2813b475e502902d4f
Jason Osborne <Jason.Osborne@hyland.com>
2020-06-01 22:01:11 -0400

    Initial commit

---
+++ src/Gradebook/GradeBook/GradeBook/Book.cs
@@ -0,0 +1,61 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradeBook
+{
+    class Book
+    {
+        private List<double> grades { get; set; }
+        private string name;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            LowGrade();
+            HighGrade();
+            Average();
+        }
+
+        public void LowGrade()
+        {
+            double lowGrade = double.MaxValue;
+            foreach (var grade in grades)
+            {
+                if (grade < lowGrade)
+                {
+                    lowGrade = grade;
+                }
+            }
+            Console.WriteLine($"Low: {lowGrade}");
+        }
+
+        public void HighGrade()
+        {
+            var highGrade = double.MinValue;
+            foreach (var grade in grades)
+            {
+                if (highGrade < grade)
+                {
+                    highGrade = grade;
+                }
+            }
+            Console.WriteLine($"High: {highGrade}");
+        }
+
+        public void Average()
+        {
+            Console.WriteLine($"Average: {grades.Average()}");
+        }
+    }
+}
---
+++ src/Gradebook/GradeBook/GradeBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Jason's Gradebook");
+            book.AddGrade(55.3);
+            book.AddGrade(68.9);
+            book.AddGrade(98.5);
+            book.AddGrade(105);
+
+            book.ShowStatistics();
+        }
+    }
+}
